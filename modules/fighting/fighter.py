005b9041b5a87bee3aea8ca3a2444132277da340
Lukas Rosendahl <lukas@recordbay.de>
2023-11-11 12:44:51 +0100

    fixed bug that returns after fights

--- modules/fighting/fighter.py
+++ modules/fighting/fighter.py
@@ -8,7 +8,6 @@ def fight(shared_variables):
     # We want it to run indefinitly
     while True:
         if helper.isGameActive():
-            hunting = False
             isChatting = shared_variables["isChatting"].value
             isFighting = shared_variables["isFighting"].value
             isWatching = shared_variables["isWatching"].value 
@@ -25,65 +24,15 @@ def fight(shared_variables):
                 isFighting = selfSet(False, shared_variables)
 
             if not isChatting and not isWatching and isFighting:
-                pinSolver = helper.isImageVisableOnScreen('assets/general/pin.png', 0.9)
-                if pinSolver:
-                    printx("Pin solver detected")
-                    x, y, width, height = pinSolver
-                    pin = helper.solvePin()
-                    if pin: 
-                        printx(f"Pin is {pin}")
-                        helper.clickAt(x, y)
-                        helper.sendMessage(pin)
-                        button_x, button_y = locator.coordinatesRelativeTo(pinSolver, diff_y=40)
-                        helper.clickAt(button_x, button_y)
-                        hunting = True
-                if hunting:
-                    hunt(shared_variables) 
-                else:
-                    printx(f"Fighting..")
-                    move_location = helper.isImageVisableOnScreen('assets/moves/thunderbolt.png')
-                    if move_location:
-                        x, y, width, height = move_location
-                    else:
-                        return
+                printx(f"Fighting..")
+                move_location = helper.isImageVisableOnScreen('assets/moves/waterfall.png')
+                if move_location:
+                    x, y, width, height = move_location
                     pyautogui.moveTo(x, y)
                     pyautogui.click()
 
 
-def hunt(shared_variables):
-    while True:
-        # We need battle check to know when its captured
-        hasBattleScreen = helper.isImageVisableOnScreen('assets/general/battle.png', 0.9)
-        if hasBattleScreen:
-            isChatting = shared_variables["isChatting"].value
-            isFighting = shared_variables["isFighting"].value
-            isWatching = shared_variables["isWatching"].value 
-            printx(f"Hunting..")
-            time.sleep(5)
-            if not isChatting and not isWatching:
-                bag_x, bag_y = locator.bag
-                helper.clickAt(bag_x, bag_y)
-                bag = [
-                    ('Ultra Ball', 'assets/general/ultra.png'),
-                    ('Great Ball', 'assets/general/great.png'),
-                    ('Master Ball', 'assets/general/master.png'),
-                    ('Starter Ball', 'assets/general/starter.png'),
-                    ('Poke Ball', 'assets/general/regular.png'),
-                ]
-                for ball, image in bag:
-                    hasBall = helper.isImageVisableOnScreen(image, 0.9)
-                    if hasBall:
-                        printx(f"Using a {ball}")
-                        x, y, width, height = hasBall
-                        helper.clickAt(x, y)
-                        break
-                    else:
-                        return None
-        else:
-            isFighting = selfSet(False, shared_variables)
-            break
-
-def selfSet(value, shared):
+def selfSet(value, shared):                
     shared["isFighting"].value = value
     return value
 
