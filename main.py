429cdff4aa588d5244fe0de8448a85acc944d7c6
Lukas Rosendahl <lukas@recordbay.de>
2023-11-11 13:13:52 +0100

    introducing hunter

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from services.helper import helper
 from modules.movement import runner
 from modules.chatting import chatter
 from modules.fighting import fighter
+from modules.fighting import hunter
 from modules.watching import watcher
 import time
 
@@ -15,16 +16,18 @@ shared_variables = {
 
 if __name__ == '__main__':
     helper.printx("Bot", "Starting..")
+    huntOption = input("Do you want to hunt ? (y/_)")
+
     helper.bringGameToFront()
     time.sleep(2)
 
     running_process = Process(target=runner.run, args=(shared_variables,))
-    fighting_process = Process(target=fighter.fight, args=(shared_variables,))
-    #chatting_process = Process(target=chatter.chat, args=(shared_variables,))
+    fighting_process = Process(target=hunter.hunt if huntOption == "y" else fighter.fight, args=(shared_variables,))
+    chatting_process = Process(target=chatter.chat, args=(shared_variables,))
     watching_process = Process(target=watcher.watch, args=(shared_variables,))
 
     # Start all processes
     running_process.start()
     fighting_process.start()
-    #chatting_process.start()
+    chatting_process.start()
     watching_process.start()
---
+++ modules/fighting/hunter.py
@@ -0,0 +1,81 @@
+import time
+import random
+import pyautogui
+from services.helper import helper
+from services.locator import locator
+
+def hunt(shared_variables):
+    # We want it to run indefinitly
+    while True:
+        if helper.isGameActive():
+            isChatting = shared_variables["isChatting"].value
+            isFighting = shared_variables["isFighting"].value
+            isWatching = shared_variables["isWatching"].value 
+            printx("Screen monitoring..")
+            hasBattleScreen = helper.isImageVisableOnScreen('assets/general/battle.png', 0.9)
+            if hasBattleScreen:
+                x, y, width, height = hasBattleScreen
+                printx(f"Battle detected")
+                isFighting = selfSet(True, shared_variables)
+            elif not hasBattleScreen and isFighting:
+                printx("Battle ended")
+                isFighting = selfSet(False, shared_variables)
+
+            if not isChatting and not isWatching and isFighting:
+                pinSolver = helper.isImageVisableOnScreen('assets/general/pin.png', 0.9)
+                if pinSolver:
+                    time.sleep(0.5)
+                    printx("Pin solver detected")
+                    x, y, width, height = pinSolver
+                    pin = helper.solvePin()
+                    if pin: 
+                        printx(f"Pin is {pin}")
+                        helper.clickAt(x, y)
+                        helper.sendMessage(pin)
+                        button_x, button_y = locator.coordinatesRelativeTo(pinSolver, diff_y=40)
+                        helper.clickAt(button_x, button_y)
+                        tryCatch()
+                else:
+                    printx("Hunting..")
+                    x, y = locator.run
+                    helper.clickAt(x, y)
+                    
+
+
+def tryCatch(shared_variables):
+    while True:
+        # We need battle check to know when its captured
+        hasBattleScreen = helper.isImageVisableOnScreen('assets/general/battle.png', 0.9)
+        if hasBattleScreen:
+            isChatting = shared_variables["isChatting"].value
+            isWatching = shared_variables["isWatching"].value 
+            printx(f"Catching..")
+            time.sleep(3)
+            if not isChatting and not isWatching:
+                bag_x, bag_y = locator.bag
+                helper.clickAt(bag_x, bag_y)
+                bag = [
+                    ('Ultra Ball', 'assets/general/ultra.png'),
+                    ('Master Ball', 'assets/general/master.png'),
+                    ('Great Ball', 'assets/general/great.png'),
+                    ('Starter Ball', 'assets/general/starter.png'),
+                    ('Poke Ball', 'assets/general/regular.png'),
+                ]
+                for ball, image in bag:
+                    hasBall = helper.isImageVisableOnScreen(image, 0.9)
+                    if hasBall:
+                        printx(f"Using a {ball}")
+                        x, y, width, height = hasBall
+                        helper.clickAt(x, y)
+                        break
+                    else:
+                        return
+        else:
+            return
+
+def selfSet(value, shared):
+    shared["isFighting"].value = value
+    return value
+
+def printx(str):
+    print(f"(Hunter): {str}")
